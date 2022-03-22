3716c56edc4c5163d2c3b3a1d121cae58270981a
LamThinhPhat <92035772+LamThinhPhat@users.noreply.github.com>
2022-03-22 11:28:02 +0700

    Add Favorite Activity, Design Comic Items

---
+++ app/src/main/java/com/androidrealm/bookhub/FavoriteListActivity.kt
@@ -0,0 +1,32 @@
+package com.androidrealm.bookhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class FavoriteListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite_list)
+        val recyclerViewFavorite = findViewById<RecyclerView>(R.id.RecyclerViewFavorite)
+
+
+        val listOfComic=ArrayList<Comic>()
+        listOfComic.add(Comic(R.drawable.amagami_cover,"Amagami"))
+        listOfComic.add(Comic(R.drawable.fechippuru_cover,"Fechippury"))
+        listOfComic.add(Comic(R.drawable.kanojo_cover,"Kanojo Okarimasu"))
+        listOfComic.add(Comic(R.drawable.komi_cover,"Komi-san"))
+        listOfComic.add(Comic(R.drawable.meika_cover,"Meika"))
+        listOfComic.add(Comic(R.drawable.doll_cover,"Sono bisque"))
+        listOfComic.add(Comic(R.drawable.mokanojo_cover,"Kanojo mo Kanojo"))
+        listOfComic.add(Comic(R.drawable.tonikaku_cover,"Tonakaku Cawaii"))
+        listOfComic.add(Comic(R.drawable.yofukashi_cover,"Yofukashi no uta"))
+
+        val adapter = ComicAdapter(listOfComic)
+        recyclerViewFavorite!!.adapter = adapter
+        recyclerViewFavorite.setHasFixedSize(true)
+        recyclerViewFavorite.layoutManager = GridLayoutManager(this,3)
+    }
+}
